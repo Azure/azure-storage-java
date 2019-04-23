c4fc3f13cfd7b8c4846a6e6db4c3451ba8874213
rickle-msft <frley@microsoft.com>
2019-04-23 14:58:33 -0700

    8.3.0 Release prep

--- microsoft-azure-storage/src/com/microsoft/azure/storage/Constants.java
+++ microsoft-azure-storage/src/com/microsoft/azure/storage/Constants.java
@@ -720,7 +720,7 @@ public final class Constants {
         /**
          * Specifies the value to use for UserAgent header.
          */
-        public static final String USER_AGENT_VERSION = "8.2.0";
+        public static final String USER_AGENT_VERSION = "8.3.0";
 
         /**
          * The default type for content-type and accept
